e02b5a58cbcbb24eac10a9b281fc18110bdaded6
Nick Yozov <n_yozov@hotmail.com>
2021-10-06 19:42:44 +0000

    add renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,27 +6,38 @@
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-    "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-    },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-}
+$(document).ready(function() {
+    const tweetData = [{
+            "user": {
+                "name": "Newton",
+                "avatars": "https://i.imgur.com/73hZDYK.png",
+                "handle": "@SirIsaac"
+            },
+            "content": {
+                "text": "If I have seen further it is by standing on the shoulders of giants"
+            },
+            "created_at": 1461116232227
+        },
+        {
+            "user": {
+                "name": "Descartes",
+                "avatars": "https://i.imgur.com/nlhLi3I.png",
+                "handle": "@rd"
+            },
+            "content": {
+                "text": "Je pense , donc je suis"
+            },
+            "created_at": 1461113959088
+        }
+    ];
 
-const datePosted = function(data) {
-    let date = timeago.format(data.created_at)
-    return date
-}
 
-console.log(datePosted(tweetData))
 
-const createTweetElement = function(data) {
-    let $tweet = `<article>
+
+
+
+    const createTweetElement = function(data) {
+        let $tweet = `<article>
     <header class ="user">
       <div class="name-div">
       <img src=${data.user.avatars}> 
@@ -37,7 +48,7 @@ const createTweetElement = function(data) {
     </header>
     <p class="tweet-example">${data.content.text}</p>
     <footer>
-      <p>${timeago.format(data.created_at)}</p>
+      <p>${timeago.format(new Date())}</p>
       <div class="fontawesome-tags">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -47,20 +58,22 @@ const createTweetElement = function(data) {
       </div>
       
     </footer>
-  </article>`
+  </article>`;
 
-    return $tweet
+        return $tweet;
 
-}
-
-
-const renderTweets = function(tweets) {
+    };
 
 
+    const renderTweets = function(tweets) {
+        for (let tweet of tweets) {
+            $('#tweets-container').prepend(createTweetElement(tweet));
+        }
+    };
 
-}
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    // Test / driver code (temporary)
+    renderTweets(tweetData);
+
+
+});
